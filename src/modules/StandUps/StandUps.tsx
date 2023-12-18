fe3c1f04259f19764ecc3150eeb42f5811c7209f
samuel kung'u <kungusamuel64@gmail.com>
2023-12-18 11:45:43 +0300

    feat(ui): handle load more button

--- src/modules/StandUps/StandUps.tsx
+++ src/modules/StandUps/StandUps.tsx
@@ -1,17 +1,23 @@
+import { Button } from "@chakra-ui/react";
 import Layout from "@/modules/Layout/Layout";
-import StandUp from "@/modules/StandUps/StandUp";
 import React from "react";
+import StandUp from "@/modules/StandUps/StandUp";
 
 export default function StandUps() {
-  let days = 10;
+  const [days, setDays] = React.useState(7);
   const today = new Date();
   const array = [];
+
+  const handleLoadMore = () => {
+    setDays((prev) => prev + 7);
+  };
 
   for (let i = 0; i <= days; i++) {
     const pastDate = new Date(today);
     pastDate.setDate(pastDate.getDate() - i);
     array.push(pastDate);
   }
+
   return (
     <Layout>
       {array.map((date) => (
@@ -19,6 +25,9 @@ export default function StandUps() {
           <StandUp date={date} />
         </React.Fragment>
       ))}
+      <Button onClick={handleLoadMore} colorScheme="facebook" variant="solid" w={{ base: "98%", lg: "90%" }} mx="auto" mt={2} mb={6}>
+        Load more
+      </Button>
     </Layout>
   );
 }
