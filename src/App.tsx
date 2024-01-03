a505538a0e0265cfca1d456ae003c6d187acd636
Samuel <kungusamuel64@gmail.com>
2024-01-03 01:58:55 -0800

    Chore/no ref/version1 (#10) (#11)
    * fix(api):  add likes field when creating an article
    
    * feat(comments): implement create, read and delete functionality
    
    * feat(comments): add edit comment functionality
    
    * fix(comments): update form value on edit comment
    
    * fix(ui) : remove unused imports
    
    * feat(api): create comments likes
    
    * feat(comments): add likes to comments
    
    * fix(types): change type naming
    
    * feat: use try-stand daily
    format
    
    * fix(api): fetch data by date
    
    * feat(ui): handle load more button
    
    * fix: filter blogs by user
    
    * fix(hook): change useeffcet dependency
    
    * fix(ui): navlink text typo
    
    ---------
    
    Co-authored-by: sharonkorir <csharonkorir@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,15 @@ import Login from "@/modules/Auth/Login";
 import PrivateRoute from "@/modules/Auth/PrivateRoute";
 import Layout from "@/modules/Layout/Layout";
 
-const AllBlogs = React.lazy(() => import("@/modules/Blogs/AllBlogs"));
-const MyBlogs = React.lazy(() => import("@/modules/Blogs/MyBlogs"));
-const Article = React.lazy(() => import("@/modules/Blogs/Article"));
+const Article = React.lazy(() => import("@/modules/StandUps/Article"));
+const StandUps = React.lazy(() => import("@/modules/StandUps/StandUps"));
 
 export default function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<Layout>{null}</Layout>}>
         <Routes>
-          <Route path="/" element={<PrivateRoute component={AllBlogs} />} />
-          <Route path="/my-blogs" element={<PrivateRoute component={MyBlogs} />} />
+          <Route path="/" element={<PrivateRoute component={StandUps} />} />
           <Route path="/article/:id" element={<PrivateRoute component={Article} />} />
 
           <Route path="/login" element={<Login />} />
